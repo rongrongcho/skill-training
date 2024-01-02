62b2f04557a0e6172dbda42bb8c3526328b912a6
Chorong Lee <mrarongsya@gmail.com>
2024-01-02 17:57:28 +0900

    01.JSX 문법
    JSX문법 3개 학습
    - className
    - 변수, 데이터 바인딩
    - style 설정

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -2,22 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  //변수 선언 , 서버에서 가져온 데이터로 가정한다.
+  let post = '강남 우동 맛집';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>블로그</h4>
+      </div>
+      {/* 중괄호 문법 ,데이터 바인딩*/}
+      {/* JSX 문법을 이용한 Style 넣기  */}
+      <h4 style={{color:'red',fontSize:'16px'}}>{post}</h4> 
     </div>
   );
 }
