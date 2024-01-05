86e51179010b9d66f5a4f0066da57995e77692db
Chorong Lee <mrarongsya@gmail.com>
2024-01-05 15:35:56 +0900

    state 변경 함수 그리고 버튼 기능개발
    따봉 버튼 클릭시 +1

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -3,40 +3,29 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  // 자료 잠깐 저장할 땐 변수,state 문법 사용
-  // state 만드는 법 
-  // 1. import{useState}
-  // 2. userState(보관할 자료)
-  // 3. let[작명,작명]
-  let post = '강남 우동 맛집'; 
-  let [글제목1,b]=useState('남자 코트 추천');
-  let [글제목2,c]=useState('강남 우동 맛집');
-  let [글제목3,d]=useState('파이썬 독학');
-  // 글제목은 state에 보관했던 자료 
-  // b는 state 변경을 도와주는 함수 
-  //distructing 문법을 사용했기 때문에 뒤에 함수가 생략되어있음
-  // let[글제목,b] =userState('남자 코트 추천',함수)
 
+  let post = '강남 우동 맛집'; 
+  let [글제목,b]=useState(['남자코트 추천','강남 우동맛집','파이썬 독학']);
+  let [따봉,따봉변경]=useState(0);
 
   return (
-    //retrun ()안에는 병렬로 태그2개 이상 기입금지 
     <div className="App">
       <div className="black-nav">
         <h4>ReactBlog</h4>
       </div>
       <div className='list'>
-        {/* 남자 코트 추천 출력 */}
-        <h4>{글제목1}</h4> 
+
+        <h4>{글제목[0]} <span onClick={()=>{따봉변경(따봉+1)}}>👍🏻</span> {따봉} </h4> 
         <p>2월 17일 발행</p>
       </div>
       <div className='list'>
-        {/* 남자 코트 추천 출력 */}
-        <h4>{글제목2}</h4> 
+
+        <h4>{글제목[1]}</h4> 
         <p>2월 17일 발행</p>
       </div>
             <div className='list'>
-        {/* 남자 코트 추천 출력 */}
-        <h4>{글제목3}</h4> 
+
+        <h4>{글제목[2]}</h4> 
         <p>2월 17일 발행</p>
       </div>
     </div>
