ff9b03df143049a89a3e355da49c1a0d9d1a285f
Chorong Lee <mrarongsya@gmail.com>
2024-02-07 17:36:34 +0900

    map 함수를 통해 반복된 html 문 축약하기

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -6,11 +6,8 @@ function App() {
 
   let post = '강남 우동 맛집'; 
   let [글제목,글제목변경]=useState(['남자코트 추천','강남 우동맛집','파이썬 독학']);
-  let [따봉,따봉변경]=useState(0);
-  //UI의 현재상태를 state로 저장
-  let [modal,setModal] =useState(false); //닫힌 상태 
-
-
+  let [따봉,따봉변경]=useState([0,0,0]);
+  let [modal,setModal] =useState(false);
 
   return (
     <div className="App">
@@ -31,27 +28,24 @@ function App() {
       }}>글제목정렬</button>
 
 
-      <div className='list'>
-        <h4>{글제목[0]} <span onClick={()=>{따봉변경(따봉+1)}}>👍🏻</span> {따봉} </h4> 
-        
-        <p>2월 17일 발행</p>
-      </div>
-      <div className='list'>
-
-        <h4>{글제목[1]}</h4> 
-        <p>2월 17일 발행</p>
-      </div>
-            <div className='list'>
-        <h4 onClick={()=>{ modal == true ? setModal(false):setModal(true)}}>{글제목[2]}</h4> 
-        <p>2월 17일 발행</p>
-      </div>
-      {/* state에 따라 UI가 어떻게 보일지 조건 작상
-          true 모달창 오픈 
-          false 모달창 닫힘 
-          기본값 false  
-      */}
       {
-        //삼함 연산자 -> 조건식 ? 참일때 실행할 코드 : 거짓일 때 실행할 코드 
+        글제목.map(function(a,i){
+          return (
+            <div className='list' key={i}>
+              <h4 onClick={()=>{
+                modal == true ? setModal(false) : setModal(true);
+              }}>{글제목[i]}<span onClick={()=>{
+                let copy = [...따봉];
+                copy[i] = copy [i] +1 ;
+                따봉변경(copy)
+              }}>👍🏻</span> {따봉[i]} </h4>  
+              <p>2월 17일 발행</p>
+            </div>
+          )
+        })
+      }
+
+      {
         modal ==true ? <Modal/> : null
       }
     </div>
