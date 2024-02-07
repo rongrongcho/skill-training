8a3081cddf8c55d20c00adeaf944784e73e39849
Chorong Lee <mrarongsya@gmail.com>
2024-02-07 17:01:37 +0900

    동적인 UI 모달창 띄우기
    1. html css로 미리 UI 디자인 설계 및 구현
    2. UI의 현재 상태를 state로 저장
    3. state에 따라서 UI가 어떻게 보일지 조건문 등으로 작성
    => 제목을 클릭하면 모달창이 껐다 켜지도록 설계 구현

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -2,11 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
-function App() { // 사실 이것도 Component
+function App() { 
 
   let post = '강남 우동 맛집'; 
   let [글제목,글제목변경]=useState(['남자코트 추천','강남 우동맛집','파이썬 독학']);
   let [따봉,따봉변경]=useState(0);
+  //UI의 현재상태를 state로 저장
+  let [modal,setModal] =useState(false); //닫힌 상태 
+
+
 
   return (
     <div className="App">
@@ -22,7 +26,7 @@ function App() { // 사실 이것도 Component
 
       <button onClick={()=>{
         let copy = [...글제목];
-        copy.sort();//가나다순으로 정렬
+        copy.sort();
         글제목변경(copy);
       }}>글제목정렬</button>
 
@@ -34,25 +38,29 @@ function App() { // 사실 이것도 Component
       </div>
       <div className='list'>
 
-        <h4>{글제목[1]}<span onClick={()=>{따봉변경(따봉+1)}}>👍🏻</span> {따봉}</h4> 
+        <h4>{글제목[1]}</h4> 
         <p>2월 17일 발행</p>
       </div>
             <div className='list'>
-
-        <h4>{글제목[2]}<span onClick={()=>{따봉변경(따봉+1)}}>👍🏻</span> {따봉}</h4> 
+        <h4 onClick={()=>{ modal == true ? setModal(false):setModal(true)}}>{글제목[2]}</h4> 
         <p>2월 17일 발행</p>
       </div>
-      {/* <Modal/> => 컴포넌트  
-        </Modal>만 써도 사용가능*/}
-      <Modal></Modal>
+      {/* state에 따라 UI가 어떻게 보일지 조건 작상
+          true 모달창 오픈 
+          false 모달창 닫힘 
+          기본값 false  
+      */}
+      {
+        //삼함 연산자 -> 조건식 ? 참일때 실행할 코드 : 거짓일 때 실행할 코드 
+        modal ==true ? <Modal/> : null
+      }
     </div>
     
   );
 }
 
-// Component 만들기 1. function 만들기 (위치: 다른 function 바깥) & 작명: 영어대문자
+
 function Modal(){
-  // Component 만들기 2. retrun()안에 html 담기
   return(
     <div className="modal">
     <h4>제목</h4>
