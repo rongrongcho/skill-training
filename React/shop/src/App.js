5d43e738605e746b4c6bff4da7b11b3d2e36cb47
Chorong Lee <mrarongsya@gmail.com>
2024-03-08 15:45:46 +0900

    project rename

--- React/shop/src/App.js
+++ React/shop/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">ToyShop</Navbar.Brand>
+          <Navbar.Brand href="#home">ShoppingMallPJ</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
