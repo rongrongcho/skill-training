0bd49e70862539b077bfe6ae70c73197858b9937
Chorong Lee <mrarongsya@gmail.com>
2024-03-11 18:17:51 +0900

    Componet로 축약하기

--- React/shop/src/App.js
+++ React/shop/src/App.js
@@ -22,32 +22,23 @@ function App() {
       <div className="main-bg"></div>
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
-            <img
-              src="https://codingapple1.github.io/shop/shoes1.jpg"
-              width={"80%"}
-            />
-            <h4>{shoes[0].title}</h4>
-            <p>{shoes[0].price}</p>
-          </div>
-          <div className="col-md-4">
-            <img
-              src="https://codingapple1.github.io/shop/shoes2.jpg"
-              width={"80%"}
-            />
-            <h4>{shoes[1].title}</h4>
-            <p>{shoes[1].price}</p>
-          </div>
-          <div className="col-md-4">
-            <img
-              src="https://codingapple1.github.io/shop/shoes3.jpg"
-              width={"80%"}
-            />
-            <h4>{shoes[2].title}</h4>
-            <p>{shoes[2].price}</p>
-          </div>
+          <Card shoes={shoes[0]} i={1} />
+          <Card shoes={shoes[1]} i={2} />
+          <Card shoes={shoes[2]} i={3} />
         </div>
       </div>
+    </div>
+  );
+}
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img
+        src={"https://codingapple1.github.io/shop/shoes" + props.i + ".jpg"}
+        width="80%"
+      />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.price}</p>
     </div>
   );
 }
