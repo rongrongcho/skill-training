dededd7b003069a7a122ccce1acd934bfcafc583
Chorong Lee <mrarongsya@gmail.com>
2024-03-11 17:51:05 +0900

    상품 정보 data.js 파일로 분리

--- React/shop/src/App.js
+++ React/shop/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar, Container, Nav, Row, Col } from "react-bootstrap";
-
 import bg from "./img/main-bg.jpg";
+import { useState } from "react";
+import data from "./data";
 
 function App() {
+  let [shoes] = useState(data);
   return (
     <div className="App">
       <Navbar bg="dark" data-bs-theme="dark">
@@ -25,24 +27,24 @@ function App() {
               src="https://codingapple1.github.io/shop/shoes1.jpg"
               width={"80%"}
             />
-            <h4>상품명</h4>
-            <p>상품설명</p>
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].price}</p>
           </div>
           <div className="col-md-4">
             <img
               src="https://codingapple1.github.io/shop/shoes2.jpg"
               width={"80%"}
             />
-            <h4>상품명</h4>
-            <p>상품설명</p>
+            <h4>{shoes[1].title}</h4>
+            <p>{shoes[1].price}</p>
           </div>
           <div className="col-md-4">
             <img
               src="https://codingapple1.github.io/shop/shoes3.jpg"
               width={"80%"}
             />
-            <h4>상품명</h4>
-            <p>상품설명</p>
+            <h4>{shoes[2].title}</h4>
+            <p>{shoes[2].price}</p>
           </div>
         </div>
       </div>
