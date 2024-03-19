97af01efd8cef6a568b072a163134125748e7b5b
Chorong Lee <mrarongsya@gmail.com>
2024-03-19 16:25:45 +0900

    페이지 추가
    index 페이지
    about 페이지

--- Nodejs/forum/server.js
+++ Nodejs/forum/server.js
@@ -4,6 +4,18 @@ app.listen(8080, () => {
   console.log("http://localhost:8080 에서 서버 실행중");
 });
 
-app.get("/", (요청, 응답) => {
-  응답.send("반갑다");
+app.get("/", function (요청, 응답) {
+  응답.sendFile(__dirname + "/index.html");
+});
+
+app.get("/news", (요청, 응답) => {
+  응답.send("오늘 비가 오네요");
+});
+
+app.get("/shop", (요청, 응답) => {
+  응답.send("쇼핑몰 페이지");
+});
+
+app.get("/about", (요청, 응답) => {
+  응답.sendFile(__dirname + "/about.html");
 });
