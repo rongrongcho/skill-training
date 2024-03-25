aefcbfc8460459bf8cda6e73a2fc2eab4ecbdda3
Chorong Lee <mrarongsya@gmail.com>
2024-03-25 19:00:15 +0900

    수정 기능 추가

--- Nodejs/forum/server.js
+++ Nodejs/forum/server.js
@@ -80,3 +80,21 @@ app.get("/detail/:id", async (요청, 응답) => {
     응답.status(404).send("잘못된 url, 비정상적인 접근입니다. ");
   }
 });
+
+app.get("/edit/:id", async (요청, 응답) => {
+  let result = await db
+    .collection("post")
+    .findOne({ _id: new ObjectId(요청.params.id) });
+  console.log(result);
+  응답.render("edit.ejs", { result: result });
+});
+
+app.post("/edit", async (요청, 응답) => {
+  await db.collection("post").updateOne(
+    {
+      _id: new ObjectId(요청.body.id),
+    },
+    { $set: { title: 요청.body.title, content: 요청.body.content } }
+  );
+  응답.redirect("/list");
+});
