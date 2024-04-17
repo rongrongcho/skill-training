052173951da67e7610cf8b335e8cfc71c1bb56b0
Chorong Lee <mrarongsya@gmail.com>
2024-04-17 14:17:04 +0900

    Update server.js

--- Nodejs/forum/server.js
+++ Nodejs/forum/server.js
@@ -37,6 +37,29 @@ app.use(passport.session());
 
 //==========================
 
+const { S3Client } = require("@aws-sdk/client-s3");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+const s3 = new S3Client({
+  region: "ap-northeast-2",
+  credentials: {
+    accessKeyId: "IAM에서 발급받은 액세스키",
+    secretAccessKey: "IAM에서 발급받은 시크릿키",
+  },
+});
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: "lcrbuket",
+    key: function (요청, file, cb) {
+      cb(null, Date.now().toString()); //업로드시 파일명 변경가능
+    },
+  }),
+});
+
+//==========================
+
 let db;
 const url = process.env.DB_URL;
 new MongoClient(url)
